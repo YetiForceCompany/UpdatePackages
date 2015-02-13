ca9bab4fd05dda699d3ee435998d9dc8375fe031
mariuszkrzaczkowski <m.krzaczkowski@yetiforce.com>
2015-02-13 08:32:30 +0100

    Update Packages YetiForceCRM from 1.2.0RC to 1.3.0RC

---
+++ YetiForce CRM 1.x.x/1.2.0RC_to_1.3.0RC/updates/files/languages/pl_pl/OSSMailView.php
@@ -0,0 +1,91 @@
+<?php
+/*+***********************************************************************************************************************************
+ * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * See the License for the specific language governing rights and limitations under the License.
+ * The Original Code is YetiForce.
+ * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
+ * All Rights Reserved.
+ *************************************************************************************************************************************/
+$languageStrings = array(
+	// Basic Strings
+	'OSSMailView'        => 'Maile',
+	'SINGLE_OSSMailView' => 'Mail firmowy',
+	'LBL_ADD_RECORD' => 'Dodaj rekord',
+	'LBL_RECORDS_LIST' => 'Lista maili',
+
+	// Blocks
+	'LBL_INFORMATION' => 'Informacje ogólne',
+
+	//Field Labels
+    'relatedid' => 'Powiązany z',
+	'yes' => 'Tak',
+	'no' => 'Nie',
+	'number' => 'Numer',
+	'From' => 'Od',
+	'To' => 'Do',
+	'CC' => 'DW',
+	'BCC' => 'UDW',
+	'Date'=>'Data',
+	'Subject'=>'Temat',
+	'Content'=>'Treść',
+	'SendType'=>'Typ',
+	'Sent'=>'Wysłane',
+	'Received'  => 'Odebrane',
+	'Internal'  => 'Wewnętrzna',
+	'Sent'  => 'Wysłane',
+	'Spam'  => 'Spam',
+	'Trash'  => 'Kosz',
+	'Fromaddress'=>'Od adresy',
+	'Toaddress'=>'Do adresy',
+	'messageid'=>'Message ID',
+	'Reply_to_email'=>'Odpowedz do',
+	'Reply_toaddress'=>'Odpowedz do adresy',
+	'Attachments_Exist'=>'Załączniki',
+	'Email_list'=>'Poczta',
+	'Widget list limit'=>'Ilośc wiadomości w bloku',
+	'_self'=>'tym samym oknie',
+	'_blank'=>'nowym oknie',
+	'List open email'=>'Po kliknięciu na link otwórz w',
+	'emailPreviewHeader'=>'Podgląd wiadomości email',
+	'Email_list'=>'Poczta',
+	'Email_list'=>'Poczta',
+    'LBL_CREATEMAIL' => 'Wyślij maila',
+    'LBL_OUTCOMING' => 'Wychodzące',
+    'LBL_INCOMING' => 'Przychodzące',
+    'LBL_INTERNAL' => 'Wewnętrzne',
+	'LBL_REPLYALLL' => 'Odpowiedz wszystkim',
+	'LBL_PRINT' => 'Drukuj',
+    
+	'LBL_VERIFY' => 'Do powiązania',
+	'LBL_REL_MOD' => 'Moduły powiazane',
+	'LBL_BindMailsOK' => 'Oznaczono maile do powiązania, przy kolejnym wywołaniu cron-a zostaną wiadomości powiązane',
+	'LBL_BindMails' => 'Przypisz maile',
+	'LBL_ChangeType' => 'Zmiana typu wiadomości',
+	'LBL_SELECT_TYPE' => 'Wybierz typ',
+	'ChangeType' => 'Zmiana typu zaznaczonych wiadomości',
+	'Alert_ChangeType_desc' => 'Wszystkie typy zaznaczonych wiadomości e-mail zostaną zmienione, bez możliwości cofnięcia zmian.',
+	'LBL_ChangeTypeOK' => 'Zmieniono typ wiadomości',
+	'Configuration' => 'Konfiguracja',
+	
+	//dashboards
+	'Graf' => 'Maile Firmowe wg typu',
+	'Today' => 'Dziś',
+	'Yesterday' => 'Wczoraj',
+	'Current week' => 'Aktualny tydzień',
+	'Previous week' => 'Poprzedni tydzień',	
+	'Current month' => 'Aktualny miesiąc',
+	'Previous month' => 'Poprzedni miesiąc',
+	
+);
+$jsLanguageStrings = array(
+	'From' => 'Od',
+	'To' => 'Do',
+	'CC' => 'DW',
+	'BCC' => 'UDW',
+	'Date'=>'Data',
+	'Subject'=>'Temat',
+	'Content'=>'Treść',
+	'JS_BIND_CONFIRM'=>'Czy na pewno chcesz powiązać zaznaczone wiadomości ?',
+);
