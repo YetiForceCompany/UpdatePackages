34e56fdaa0ee7141b516979b133eea4e54c8922d
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2015-08-10 11:02:56 +0200

    Added empty Update Packages YetiForceCRM from 2.1.0 to 2.2.0

---
+++ YetiForce CRM 2.x.x/2.1.0_to_2.2.0/updates/init.php
@@ -0,0 +1,45 @@
+<?php
+/* +***********************************************************************************************************************************
+ * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * See the License for the specific language governing rights and limitations under the License.
+ * The Original Code is YetiForce.
+ * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
+ * All Rights Reserved.
+ * *********************************************************************************************************************************** */
+require_once 'modules/com_vtiger_workflow/include.inc';
+require_once 'modules/com_vtiger_workflow/tasks/VTEntityMethodTask.inc';
+require_once 'modules/com_vtiger_workflow/VTEntityMethodManager.inc';
+require_once('include/events/include.inc');
+include_once('vtlib/Vtiger/Module.php');
+
+class YetiForceUpdate
+{
+
+	var $package;
+	var $modulenode;
+	var $return = true;
+	var $filesToDelete = [];
+
+	function YetiForceUpdate($modulenode)
+	{
+		$this->modulenode = $modulenode;
+	}
+
+	function preupdate()
+	{
+		//$this->package->_errorText = 'Errot';
+		return true;
+	}
+
+	function update()
+	{
+	}
+
+	function postupdate()
+	{
+		return true;
+	}
+
+}
