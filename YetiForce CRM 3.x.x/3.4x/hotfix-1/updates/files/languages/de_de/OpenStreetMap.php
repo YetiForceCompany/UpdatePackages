95cc66cf4b9c4622da50459e8a0a30ca5d929fb5
Tomasz Kur <t.kur@yetiforce.com>
2016-10-11 13:35:18 +0200

    Updated package 3.4x-hotfix-1

---
+++ YetiForce CRM 3.x.x/3.4x/hotfix-1/updates/files/languages/de_de/OpenStreetMap.php
@@ -0,0 +1,20 @@
+<?php
+/* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
+$languageStrings = [
+	'LBL_GROUP_BY' => 'Gruppe',
+	'LBL_GROUP_NEIGHBOURS' => 'Gruppe benachbarter Elemente',
+	'LBL_NOT_FOUND_PLACE' => 'Ort nicht gefunden',
+	'LBL_SEARCH_VALUE_DESCRIPTION' => 'Stadt, Straße, Hausnummer ...',
+	'LBL_IN_RADIUS' => 'Innerhalb der Radius [km]',
+	'LBL_CALCULATE_TRACK' => ' Weginformation einstellen',
+	'LBL_START' => 'Start',
+	'LBL_END' => 'Ende',
+	'LBL_CALCULATE_ROUTE_HEADER_BLOCK' => ' Weginformation einstellen',
+	'LBL_DISTANCE' => 'Entfernung [km]',
+	'LBL_TRAVEL_TIME' => 'Zeit [min]',
+	'LBL_INSTRUCTION' => 'Manual',
+	'LBL_SEARCH_IN_RADIUS' => 'Search in radius',
+	'LBL_COPY_TO_CLIPBOARD' => 'Copy to clipboard',
+	'LBL_CLIPBOARD' => 'Clipboard',
+	'LBL_INDIRECT_POINT' => 'Indirect point',
+];
