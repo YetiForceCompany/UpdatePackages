2ef85f35db619355cea2b1771e8807916ea97d7b
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-08-01 09:34:34 +0200

    Update files 4.1.0

--- YetiForce CRM 4.x.x/4.0.0_to_4.1.0/updates/files/config/version.php
+++ YetiForce CRM 4.x.x/4.0.0_to_4.1.0/updates/files/config/version.php
@@ -1,9 +1,9 @@
 <?php
 return [
-	'appVersion' => '4.0.521',
-	'patchVersion' => '2017.07.28',
+	'appVersion' => '4.1.0',
+	'patchVersion' => '2017.07.31',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.30',
+	'lib_roundcube' => '0.0.31',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_gantt' => '0.0.1',
 ];
