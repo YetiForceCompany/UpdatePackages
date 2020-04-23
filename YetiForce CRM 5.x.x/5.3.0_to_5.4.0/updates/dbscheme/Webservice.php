61f174be82f6704e2d2f7fce20544a5a44342cf4
Arek Solek <arkadiusz_s9887@wp.pl>
2020-04-23 10:03:17 +0200

    Update package - Webservice scheme

--- YetiForce CRM 5.x.x/5.3.0_to_5.4.0/updates/dbscheme/Webservice.php
+++ YetiForce CRM 5.x.x/5.3.0_to_5.4.0/updates/dbscheme/Webservice.php
@@ -16,6 +16,21 @@ class Webservice extends \App\Db\Importers\Base
 	public function scheme()
 	{
 		$this->tables = [
+			'w_#__portal_user' => [
+				'columns' => [
+					'password_t' => $this->stringType(500),
+				],
+				'engine' => 'InnoDB',
+				'charset' => 'utf8'
+			],
+			'w_#__servers' => [
+				'columns' => [
+					'pass' => $this->stringType(500),
+					'api_key' => $this->stringType(500)->notNull(),
+				],
+				'engine' => 'InnoDB',
+				'charset' => 'utf8'
+			],
 		];
 		$this->foreignKey = [
 		];
