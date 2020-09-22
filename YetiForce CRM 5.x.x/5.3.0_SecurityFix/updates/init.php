bfe568e7286920497b377f8a571b07647926e351
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2020-09-22 13:10:46 +0200

    Security fix for 5.3

---
+++ YetiForce CRM 5.x.x/5.3.0_SecurityFix/updates/init.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * YetiForceUpdate Class.
+ *
+ * @copyright YetiForce Sp. z o.o.
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+
+/**
+ * YetiForceUpdate Class.
+ */
+class YetiForceUpdate
+{
+	/**
+	 * @var object
+	 */
+	public $modulenode;
+
+	/**
+	 * Fields to delete.
+	 *
+	 * @var string[]
+	 */
+	public $filesToDelete = [];
+
+	/**
+	 * Constructor.
+	 *
+	 * @param object $modulenode
+	 */
+	public function __construct($modulenode)
+	{
+		$this->modulenode = $modulenode;
+		
+	}
+
+	/**
+	 * Preupdate.
+	 */
+	public function preupdate()
+	{
+		$minTime = 600;
+		if (ini_get('max_execution_time') < $minTime || ini_get('max_input_time') < $minTime) {
+			$this->package->_errorText = 'The server configuration is not compatible with the requirements of the upgrade package.' . PHP_EOL;
+			$this->package->_errorText .= 'Please have a look at the list of errors:';
+			if (ini_get('max_execution_time') < $minTime) {
+				$this->package->_errorText .= PHP_EOL . 'max_execution_time = ' . ini_get('max_execution_time') . ' < ' . $minTime;
+			}
+			if (ini_get('max_input_time') < $minTime) {
+				$this->package->_errorText .= PHP_EOL . 'max_input_time = ' . ini_get('max_input_time') . ' < ' . $minTime;
+			}
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Update.
+	 */
+	public function update()
+	{
+		$fielName = \App\Security\Dependency::CACHE_FILE_NAME;
+		if(file_exists($fielName)){
+			unlink($fielName);
+		}
+		return true;
+	}
+
+	/**
+	 * Postupdate.
+	 */
+	public function postupdate()
+	{
+		return true;
+	}
+}
