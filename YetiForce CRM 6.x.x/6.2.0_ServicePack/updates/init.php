56b40af50e5695f082d264faab71bbd33f59e38f
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2021-07-08 10:27:41 +0200

    6.2.0_ServicePack_v1

---
+++ YetiForce CRM 6.x.x/6.2.0_ServicePack/updates/init.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * YetiForceUpdate Class.
+ *
+ * @package   YetiForce.UpdatePackages
+ *
+ * @copyright YetiForce Sp. z o.o.
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
+ */
+
+/**
+ * YetiForceUpdate Class.
+ */
+class YetiForceUpdate
+{
+	/**
+	 * @var string
+	 */
+	public $logFile = 'cache/logs/updateLogsTrace.log';
+	/**
+	 * @var \vtlib\PackageImport
+	 */
+	public $package;
+
+	/**
+	 * @var object
+	 */
+	public $moduleNode;
+
+	/**
+	 * Fields to delete.
+	 *
+	 * @var string[]
+	 */
+	public $filesToDelete = [];
+
+	/**
+	 * Constructor.
+	 *
+	 * @param object $moduleNode
+	 */
+	public function __construct($moduleNode)
+	{
+		$this->moduleNode = $moduleNode;
+	}
+
+	/**
+	 * Logs.
+	 *
+	 * @param string $message
+	 */
+	public function log($message)
+	{
+		$fp = fopen($this->logFile, 'a+');
+		fwrite($fp, $message . PHP_EOL);
+		fclose($fp);
+	}
+
+	/**
+	 * Preupdate.
+	 */
+	public function preupdate()
+	{
+		return true;
+	}
+
+	/**
+	 * Update.
+	 */
+	public function update()
+	{
+	}
+
+	/**
+	 * Postupdate.
+	 */
+	public function postupdate()
+	{
+		\vtlib\Functions::recurseDelete('cache/templates_c');
+		\App\Cache::clear();
+		\App\Cache::resetOpcache();
+		return true;
+	}
+}
