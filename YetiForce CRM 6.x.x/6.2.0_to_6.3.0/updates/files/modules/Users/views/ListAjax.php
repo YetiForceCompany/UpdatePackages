ce48893c30f8828121cf187ca0946accf970c990
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-12-10 11:40:15 +0100

    Improved 6.2.0_to_6.3.0

---
+++ YetiForce CRM 6.x.x/6.2.0_to_6.3.0/updates/files/modules/Users/views/ListAjax.php
@@ -0,0 +1,24 @@
+<?php
+/* +**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.1
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * ********************************************************************************** */
+
+class Users_ListAjax_View extends Users_List_View
+{
+	use \App\Controller\ExposeMethod;
+	use App\Controller\ClearProcess;
+
+	/** {@inheritdoc} */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->exposeMethod('getListViewCount');
+		$this->exposeMethod('getRecordsCount');
+		$this->exposeMethod('getPageCount');
+	}
+}
