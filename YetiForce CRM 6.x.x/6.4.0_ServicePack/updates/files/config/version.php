6922525a9e65f3ce142437678c105fe94f6bf898
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2022-08-25 10:36:12 +0200

    6.4.0_ServicePack v1

---
+++ YetiForce CRM 6.x.x/6.4.0_ServicePack/updates/files/config/version.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'appVersion' => '6.4.0',
+	'patchVersion' => '2022.08.25',
+	'lib_roundcube' => '0.3.1',
+];
---
+++ YetiForce CRM 6.x.x/6.4.0_ServicePack/updates/init.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * YetiForceUpdate Class.
+ *
+ * @package   YetiForce.UpdatePackages
+ *
+ * @copyright YetiForce Sp. z o.o.
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+ 
+//  SHA-1: 57b3f1a9b29bfe0030829840ec85cd51c5be89e6
+
+/**
+ * YetiForceUpdate Class.
+ */
+class YetiForceUpdate
+{
+	/**
+	 * @var string
+	 */
+	public $logFile = 'cache/logs/updateLogsTrace.log';
+	/**
+	 * @var \vtlib\PackageImport
+	 */
+	public $package;
+
+	/**
+	 * @var object
+	 */
+	public $moduleNode;
+
+	/**
+	 * Fields to delete.
+	 *
+	 * @var string[]
+	 */
+	public $filesToDelete = [];
+
+	/**
+	 * Constructor.
+	 *
+	 * @param object $moduleNode
+	 */
+	public function __construct($moduleNode)
+	{
+		$this->moduleNode = $moduleNode;
+	}
+
+	/**
+	 * Logs.
+	 *
+	 * @param string $message
+	 */
+	public function log($message)
+	{
+		$fp = fopen($this->logFile, 'a+');
+		fwrite($fp, $message . PHP_EOL);
+		fclose($fp);
+	}
+
+	/**
+	 * Preupdate.
+	 */
+	public function preupdate()
+	{
+		return true;
+	}
+
+	/**
+	 * Update.
+	 */
+	public function update()
+	{
+	
+	}
+
+
+	/**
+	 * Postupdate.
+	 */
+	public function postupdate()
+	{
+		\vtlib\Functions::recurseDelete('cache/templates_c');
+		\App\Cache::clear();
+		\App\Cache::resetOpcache();
+		return true;
+	}
+}
