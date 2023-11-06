5fb490cad0dff0a122efe04783dd2bc67a22bda2
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2023-11-06 14:07:23 +0100

    6.4.0_to_6.5.0 initial pack

---
+++ YetiForce CRM 6.x.x/6.4.0_to_6.5.0/updates/deleteFiles.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package   YetiForce.UpdatePackages
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license   YetiForce Public License 6.5 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
+ */
+return [
+
+];
